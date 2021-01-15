d502980314e73f921aa80917168f213f3436165b
GuillaumeDua <guillaume.dua@gmail.com>
2021-01-15 23:44:44 +0100

    [2021/IBA_using_concepts] paste tests from godbolt

---
+++ 2021/IBA_using_concepts/example.hpp
@@ -0,0 +1,186 @@
+#include <utility>
+#include <type_traits>
+#include <functional>
+
+// SFINAE, detection idiom, contracts
+//
+// Contract example : feline
+// - default constructible
+// - mammal
+//      Animal vertébré
+//          Animal
+//              êtres vivants
+//              hétérotrophes : se nourrissent de substances organiques
+//          vertebrate
+//              has_spine
+//      température constante
+//      respirant par des poumons
+//      [skipped] : système nerveux central développé
+//      les femelles portent des mamelles
+// - predator
+//      Hunt(prey)
+// - prey
+//      Hunted(predator)
+
+// Animal examples
+//  - spineless : mosquitos, molluscs (snails, ...)
+//  - Not feline : dogs
+//  - not const temperature : snakes
+//  - no udders : birds
+
+// Points :
+// - static polymorphism
+// - type erasure
+// - zero-cost abstration
+// - no tight coupling (classes, CMake targets, ...)
+//  - share contracts, not classes
+//  - no forward-declarations
+
+// todo : check perfs
+
+namespace using_inheritance
+{
+    struct animal
+    {
+        virtual void behave() = 0;
+    };
+    class vertebrate : animal
+    {
+        struct spine_type{} spine;
+    };
+    enum gender_type { male, female };
+    struct gendered
+    {
+        const gender_type gender;
+    };
+
+    struct predator;
+    struct prey
+    {
+        virtual void hunted_by(const predator&) = 0;
+    };
+    struct predator
+    {
+        virtual void hunt(prey&) = 0;
+    };
+
+    struct mammal : vertebrate, gendered
+    {
+        const int temperature;
+
+        const auto has_udders()
+        {
+            return gender == gender_type::female;
+        } 
+
+        void breathe()
+        {
+            lungs.use();
+        }
+
+    private:
+        struct lungs_type{ void use(){}; } lungs;
+    };
+    struct feline : mammal, predator
+    {};
+}
+namespace using_cpp17
+{
+    template <typename T>
+    constexpr static const auto can_fluctuate = std::is_const_v<T::value>;
+
+    // gennder-specific : crtp ?
+}
+namespace using_contracts
+{
+    template <typename T>
+    concept animal = requires(T & value) {
+        { value.behave() };
+    };
+    template <typename T>
+    concept vertebrate = requires(const T & value) {
+        animal<T>;
+        { value.spine };
+    };
+    template <typename T>
+    concept gendered = requires(const T & value) {
+        { value.gender };
+    };
+    template <typename T, typename predator_type>
+    concept prey = requires(T & value) {
+        { value.hunted_by(std::declval<const predator_type&>()) };
+        { std::declval<predator_type&>().hunt(value) };
+    };
+    template <typename T, typename prey_type>
+    concept predator = requires(T & value) {
+        { value.hunt(std::declval<prey_type&>()) };
+        { std::declval<prey_type&>().hunted_by(std::declval<const T &>()) };
+    };
+
+    template <typename T>
+    concept is_iterable = requires(T value) {
+        { value.begin() } -> std::same_as<decltype(value.end())>;
+    };
+
+    template <typename T>
+    concept mammal = requires(T & value) {
+        { value.temperature } -> std::convertible_to<int>;
+        std::is_const_v<decltype(std::declval<T&>().temperature)>;
+        { value.udders }
+    };
+
+    // todo : crtp -> gender_specific
+
+    struct mammal : vertebrate, gendered
+    {
+        const int temperature;
+
+        const auto has_udders()
+        {
+            return gender == gender_type::female;
+        } 
+
+        void breathe()
+        {
+            lungs.use();
+        }
+
+    private:
+        struct lungs_type{ void use(){}; } lungs;
+    };
+    struct feline : mammal, predator
+    {};
+}
+
+
+namespace impl_1
+{
+    template <typename T>
+    using IsNotReference = typename std::enable_if<!std::is_reference<T>::value>::type;
+
+    template <typename T, typename = IsNotReference<T>>
+    void do_stuff(T && arg)
+    {}
+    void test()
+    {
+        do_stuff(int{42});
+        int i = 42;
+        // do_stuff(i); // error: no matching function for call to 'do_stuff(int&)'
+    }
+}
+namespace impl_2
+{
+    template <typename T>
+    struct IsNotReference : std::negation<std::is_reference<T>>{ };
+    template <typename T>
+    constexpr static auto IsNotReference_v = IsNotReference<T>::value;
+
+    template <typename T, typename = std::enable_if_t<IsNotReference_v<T>>>
+    void do_stuff(T && arg)
+    {}
+
+    // template< class, class = void>
+    // struct IsNotReference : std::false_type { };
+    //  template <class T>
+    // struct IsNotReference<T, std::void_t<typename T::type>> : std::true_type { };
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# papers_code_examples
-Examples for papers published on Gist.Github.com/GuillaumeDua, LinkedIn and Reddit
+# Code examples for my publications
+
+Code examples for papers published on [Gist](https://gist.github.com/GuillaumeDua),  
+then republished on [LinkedIn](https://www.linkedin.com/in/guillaume-dua-541a40144/) and [Reddit](https://www.reddit.com/user/Guillaume_Guss_Dua).
