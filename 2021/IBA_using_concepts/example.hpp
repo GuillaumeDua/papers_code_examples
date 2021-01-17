f35e8b341d4b303ae46e9d76d1bb45bb6c55a116
GuillaumeDua <guillaume.dua@gmail.com>
2021-01-17 18:38:21 +0100

    [2021/IBA_using_concepts] WIP : fix gendered and predator concepts

--- 2021/IBA_using_concepts/example.hpp
+++ 2021/IBA_using_concepts/example.hpp
@@ -1,6 +1,7 @@
 #include <utility>
 #include <type_traits>
 #include <functional>
+#include <array>
 
 namespace using_inheritance
 {
@@ -71,33 +72,35 @@ namespace using_contracts
         value.spine;
     };
 
-    template <typename T, typename predator_type>
-    concept prey = requires(T & value) {
-        { value.hunted_by(std::declval<const predator_type&>()) };
-        //{ std::declval<predator_type&>().hunt(value) };
-    };
-
-    template <typename T, typename prey_type>
-    concept predator = requires(T & value) {
+    template <typename predator_type, typename prey_type>
+    concept predator_of = requires(predator_type & value) {
         { value.hunt(std::declval<prey_type&>()) };
-        //{ std::declval<prey_type&>().hunted_by(std::declval<const T &>()) };
+        { std::declval<prey_type&>().hunted_by(std::declval<const predator_type &>()) };
     };
+    template <typename prey_type, typename predator_type>
+    concept prey_of = predator_of<predator_type, prey_type>;
 
     template <typename T>
-    concept gendered = requires(T) {
-        T::gender_value;
+    concept gendered = requires(T value) {
+        { std::is_enum_v<decltype(T::gender_value)> };
+        { T::gender_value } -> std::same_as<const typename T::gender_type &>;
     };
     template <typename T>
     concept female = gendered<T> && requires(T) {
-        std::is_enum_v<decltype(T::gender_value)>;
         T::gender_value == decltype(T::gender_value)::female;
     };
+    template <typename T>
+    concept not_female = not female<T>;
 
     template <typename T>
     concept iterable = requires(T value) {
         { value.begin() } -> std::same_as<decltype(value.end())>;
     };
-
+    template <typename T>
+    concept has_udders = requires(const T value) {
+        { value.udders } -> iterable;
+        { std::size(value.udders) > 0 };
+    };
     template <typename T>
     concept has_constant_temperature = requires(T & value) {
         { value.temperature } -> std::convertible_to<int>;
@@ -111,23 +114,30 @@ namespace using_contracts
         && requires(T & value) {
         {
             not female<T> ||
-            (female<T> && requires { value.udders -> iterable; })
-        }; // or std::size | std::array::size > 0
+            (female<T> && has_udders<T>)
+        };
         { value.breathe() };
     };
 
     template <class T, class prey_type>
-    concept feline = mammal<T> && predator<T, prey_type>;
+    concept feline = mammal<T> && predator_of<T, prey_type>;
 
     // ---------- Impls
 
     template <class T, class predator_type>
-    concept rodent = mammal<T> && prey<T, predator_type>;
+    concept rodent = mammal<T> && prey_of<T, predator_type>;
 
     struct mouse
     {
         enum gender_type { unknown };
         constexpr static auto gender_value = gender_type::unknown;
+
+        // prey requirements ...
+        template <typename predator_type>
+        void hunted_by(const predator_type &)
+        {
+            static_assert(prey_of<mouse, predator_type>);
+        }
 
         // animal requirements ...
         void behave(){}
@@ -136,16 +146,11 @@ namespace using_contracts
         struct spine_type{};
         spine_type spine;
 
-        // prey requirements ...
-        template <predator<mouse> predator_type>
-        void hunted_by(const predator_type &){}
-
+        // mammals requirements ...
+        void breathe(){}
         // has_constant_temperature
         const int temperature = 35;
     };
-    // static_assert(rodent<mouse>);
-
-    // todo : crtp -> gender_specific
 
     struct cat
     {
@@ -161,11 +166,10 @@ namespace using_contracts
         spine_type spine;
 
         // predator requirements ...
-        // template <prey<cat> prey_type> // -> circular dependency
         template <typename prey_type>
         void hunt(prey_type &)
         {
-            static_assert(prey<prey_type, cat>);
+            static_assert(predator_of<cat, prey_type>);
         }
 
         // has_constant_temperature
@@ -175,11 +179,19 @@ namespace using_contracts
         void breathe(){}
 
     };
+    static_assert(rodent<mouse, cat>);
     static_assert(feline<cat, mouse>);
 
-    template <feline<cat> feline_type>
-    void use_feline()
+    template <feline<mouse> feline_type>
+    void use_feline(feline_type some_feline)
     {
+        mouse some_mouse;
+        some_feline.hunt(some_mouse);
+    }
 
+    void test()
+    {
+        cat some_cat;
+        use_feline(some_cat);
     }
 }
